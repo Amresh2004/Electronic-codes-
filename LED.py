a31b4388278b5b7fa7a6b1b6afcb251ad4414444
Amresh2004 <150937631+Amresh2004@users.noreply.github.com>
2024-04-21 19:22:14 +0530

    Update

---
+++ LED.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO
+import time
+LED1=14
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+for i in range(10):
+GPIO.setup(LED1,GPIO.OUT)
+GPIO.output(LED1,True)
+time.sleep(1)
+GPIO.output(LED1,False)
+time.sleep(1)
+GPIO.cleanup
