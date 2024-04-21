d3a9662f4bf083348f1b3bd2f501c2b0f4e62604
Amresh2004 <150937631+Amresh2004@users.noreply.github.com>
2024-04-21 19:22:43 +0530

    Update LED.py

--- LED.py
+++ LED.py
@@ -4,9 +4,9 @@ LED1=14
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 for i in range(10):
-GPIO.setup(LED1,GPIO.OUT)
-GPIO.output(LED1,True)
-time.sleep(1)
-GPIO.output(LED1,False)
-time.sleep(1)
+    GPIO.setup(LED1,GPIO.OUT)
+    GPIO.output(LED1,True)
+    time.sleep(1)
+    GPIO.output(LED1,False)
+    time.sleep(1)
 GPIO.cleanup
