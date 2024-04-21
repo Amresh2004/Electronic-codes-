c2f1ff74e23e77b8e078de8f84962beca766ded7
Amresh2004 <150937631+Amresh2004@users.noreply.github.com>
2024-04-21 19:57:34 +0530

    Update transmitter(TX).py

--- transmitter(TX).py
+++ transmitter(TX).py
@@ -1,20 +1,18 @@
-// Xbee transmitter Code
 void setup() 
 {
-// put your setup code here, to run once:
-Serial.begin(9600);
+   Serial.begin(9600);
 }
 void loop() 
 {
-float TempLM35,Pot;
-char sign = 223;
-TempLM35 = analogRead(A0);
-delay(100);
-Pot = analogRead(A1);
-Serial.print(TempLM35* 0.48828125);
-Serial.print(sign);
-Serial.print("C ");
-Serial.print(Pot* 0.48828125);
-Serial.print("mV\n");
-delay(1000);
+  float TempLM35,Pot;
+  char sign = 223;
+  TempLM35 = analogRead(A0);
+  delay(100);
+  Pot = analogRead(A1);
+  Serial.print(TempLM35* 0.48828125);
+  Serial.print(sign);
+  Serial.print("C ");
+  Serial.print(Pot* 0.48828125);
+  Serial.print("mV\n");
+  delay(1000);
 }
